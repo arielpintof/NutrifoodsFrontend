1e5914479eedb39b1e77431641e9c8dc4b8c3edd
arielpintof <64876168+arielpintof@users.noreply.github.com>
2022-11-29 23:10:49 -0300

    add request mealplan from a guest user

--- Data/Dto/GuestUserDto.cs
+++ Data/Dto/GuestUserDto.cs
@@ -2,9 +2,9 @@
 
 public class GuestUserDto
 {
-    public int Height { get; set; }
-    public int Weight { get; set; }
-    public int Age { get; set; }
+    public int? Height { get; set; }
+    public int? Weight { get; set; }
+    public int? Age { get; set; }
     public string Gender { get; set; } = string.Empty;
     public string PhysicalActivity { get; set; } = null!;
 }
