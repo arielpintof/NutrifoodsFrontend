ab630332dcc30c15af9f555acab28a21bb3133a7
arielpintof <64876168+arielpintof@users.noreply.github.com>
2022-11-30 22:41:51 -0300

    add info messages for futures tooltips text

---
+++ UtilsFolder/ToolTip/InfoMessage.cs
@@ -0,0 +1,7 @@
+﻿namespace NutrifoodsFrontend.UtilsFolder.ToolTip
+{
+    public static class InfoMessage
+    {
+        public static string DailyConfiguration = "Modifica la cantidad de comidas y sus contundencias";
+    }
+}
