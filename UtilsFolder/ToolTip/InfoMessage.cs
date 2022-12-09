5499b0109e2e62f3dd989d6190fe72decebd9cca
arielpintof <64876168+arielpintof@users.noreply.github.com>
2022-12-09 19:17:43 -0300

    Update

--- UtilsFolder/ToolTip/InfoMessage.cs
+++ UtilsFolder/ToolTip/InfoMessage.cs
@@ -4,6 +4,11 @@
     {
         public const string DailyConfiguration = "Modifica la cantidad de comidas y sus contundencias";
 
-        public const string UserWarning = "Tus datos personales y métricas corporales estan incompletos";
+        public const string UserWarning = "Tu proceso de registro está incompleto";
+
+
+        public const string IndexImage =
+            "https://o.remove.bg/downloads/cad9602f-c1c8-4a3b-82ab-98f49c7b4c85/png-clipart-fruit-" +
+            "healthy-diet-food-healthy-girl-natural-foods-food-removebg-preview.png";
     }
 }
