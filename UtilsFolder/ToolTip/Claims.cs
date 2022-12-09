1b99bca21ead749d18535e395d65e60972179a4c
arielpintof <64876168+arielpintof@users.noreply.github.com>
2022-12-08 22:21:59 -0300

    Update Claims.cs

--- UtilsFolder/ToolTip/Claims.cs
+++ UtilsFolder/ToolTip/Claims.cs
@@ -3,5 +3,6 @@
     public static class Claims
     {
         public const string NameIdentifier = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier";
+        public const string Email = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress";
     }
 }
