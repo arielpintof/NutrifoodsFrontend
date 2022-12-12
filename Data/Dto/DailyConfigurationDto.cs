855fc8717059fd8ecd9af6b81d5cdb8cbe132b76
arielpintof <64876168+arielpintof@users.noreply.github.com>
2022-12-11 22:21:42 -0300

    Create DailyConfigurationDto.cs

---
+++ Data/Dto/DailyConfigurationDto.cs
@@ -0,0 +1,10 @@
+﻿using NutrifoodsFrontend.UtilsFolder.Enums;
+
+namespace NutrifoodsFrontend.Data.Dto;
+
+public class DailyConfigurationDto
+{
+    public bool IsLunchFilling { get; set; }
+    public Satiety Breakfast { get; set; }
+    public Satiety Dinner { get; set; }
+}
