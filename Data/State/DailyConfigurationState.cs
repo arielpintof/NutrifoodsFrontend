79c153b853d19469ac0d6f832af355257144833a
arielpintof <64876168+arielpintof@users.noreply.github.com>
2022-12-11 22:22:00 -0300

    Create DailyConfigurationState.cs

---
+++ Data/State/DailyConfigurationState.cs
@@ -0,0 +1,23 @@
+﻿using NutrifoodsFrontend.Data.Dto;
+using NutrifoodsFrontend.UtilsFolder.Enums;
+
+namespace NutrifoodsFrontend.Data.State;
+
+public class DailyConfigurationState
+{
+    private DailyConfigurationDto _savedDailyConfiguration;
+
+    public DailyConfigurationDto Property
+    {
+        get => _savedDailyConfiguration;
+        set
+        {
+            _savedDailyConfiguration = value;
+            NotifyStateChanged();
+        }
+    }
+
+    public event Action? OnChange;
+
+    private void NotifyStateChanged() => OnChange?.Invoke();
+}
