1ab0d9dcd99d4ae7e8a6998c8c7439fd33d06d09
arielpintof <64876168+arielpintof@users.noreply.github.com>
2022-12-14 20:53:39 -0300

    Create WeekPlanState.cs

---
+++ Data/State/WeekPlanState.cs
@@ -0,0 +1,33 @@
+﻿using NutrifoodsFrontend.Data.Dto;
+
+namespace NutrifoodsFrontend.Data.State
+{
+    public class WeekPlanState
+    {
+        private List<DailyMealPlanDto> _weekPlan = new();
+
+        public List<DailyMealPlanDto> Property
+        {
+            get => _weekPlan;
+            set
+            {
+                _weekPlan = value;
+                
+                NotifyStateChanged();
+            }
+        }
+        public void AddDailyMealPlan(DailyMealPlanDto dailyMealPlan)
+        {
+            if(_weekPlan.Count >= 7)
+            {
+                throw new Exception("La lista de planes ya está completa (7 elementos)");
+            }
+
+            _weekPlan.Add(dailyMealPlan);
+            NotifyStateChanged();
+        }
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
