bd3150bc4c0a7bfc4e893b6cdabcf8d3c2ae2bfe
arielpintof <64876168+arielpintof@users.noreply.github.com>
2022-12-15 22:50:59 -0300

    Update GuestUserDto.cs

--- Data/Dto/GuestUserDto.cs
+++ Data/Dto/GuestUserDto.cs
@@ -5,6 +5,6 @@ public class GuestUserDto
     public int Height { get; set; }
     public double Weight { get; set; }
     public int Age { get; set; }
-    public string Gender { get; set; }
+    public string? Gender { get; set; }
     public string PhysicalActivity { get; set; } = null!;
 }
