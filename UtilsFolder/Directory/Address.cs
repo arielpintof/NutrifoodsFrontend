3aa66e7580b3b262e5c9d7f2ac72fc35651518de
arielpintof <64876168+arielpintof@users.noreply.github.com>
2022-12-18 01:54:39 -0300

    Create Address.cs

---
+++ UtilsFolder/Directory/Address.cs
@@ -0,0 +1,8 @@
+﻿namespace NutrifoodsFrontend.UtilsFolder.Directory;
+
+public class Address
+{
+    public const string BaseAddress = "http://localhost:5000/";
+    public const string BodyMeasuresAddress = BaseAddress + "BodyMeasures/";
+    
+}
