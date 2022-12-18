b029cf3bb36b54c61ddf742a80b152a5a8235605
arielpintof <64876168+arielpintof@users.noreply.github.com>
2022-12-18 16:21:50 -0300

    Update: guest metrics code isolation

---
+++ Pages/Components/Metrics/GuestMetrics.razor.cs
@@ -0,0 +1,29 @@
+﻿using MudBlazor;
+using NutrifoodsFrontend.UtilsFolder.Enums;
+using NutrifoodsFrontend.UtilsFolder.Nutrition;
+
+namespace NutrifoodsFrontend.Pages.Components.Metrics;
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public partial class GuestMetrics
+{
+    private MudForm? _form;
+    private bool _success;
+    private string[] _errors = { };
+    private double? _weightValue;
+    private int? _heightValue;
+    private int? _ageValue;
+    private GenderEnum? _genderValue;
+    private PhysicalActivityEnum? _physicalValue;
+    
+    protected override void OnInitialized() => UserEnergyState.OnChange += StateHasChanged;
+
+    private void ChangePropertyValue() => UserEnergyState.Property = TotalMetabolicRate.Calculate(_genderValue!, (double) _weightValue!, 
+        (int)_heightValue!, (int)_ageValue!, PhysicalActivityEnum.Active);
+    
+    private static IEnumerable<GenderEnum> GenderEnumList => GenderEnum.NonNullValues;
+    
+    private static IEnumerable<PhysicalActivityEnum> PhysicalActivityEnumList => PhysicalActivityEnum.NonNullValues;
+
+    void IDisposable.Dispose() => UserEnergyState.OnChange -= StateHasChanged;
+}
