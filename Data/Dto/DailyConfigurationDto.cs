382ad5cff6d7573def487043aded8ad9a1a436a0
arielpintof <64876168+arielpintof@users.noreply.github.com>
2022-12-18 16:22:29 -0300

    Add: brunch and linner bool values to dailyConfigurationDto

--- Data/Dto/DailyConfigurationDto.cs
+++ Data/Dto/DailyConfigurationDto.cs
@@ -7,4 +7,6 @@ public class DailyConfigurationDto
     public bool IsLunchFilling { get; set; }
     public Satiety Breakfast { get; set; }
     public Satiety Dinner { get; set; }
+    public bool Brunch { get; set; }
+    public bool Linner { get; set; }
 }
