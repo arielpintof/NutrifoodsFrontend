b82b11b7ea38252f0798fbd4b312ef7602d93ea1
arielpintof <64876168+arielpintof@users.noreply.github.com>
2022-12-18 16:29:14 -0300

    Update Address.cs

--- UtilsFolder/Directory/Address.cs
+++ UtilsFolder/Directory/Address.cs
@@ -3,6 +3,7 @@
 public class Address
 {
     public const string BaseAddress = "http://localhost:5000/";
-    public const string BodyMeasuresAddress = BaseAddress + "BodyMeasures/";
+    public const string BodyMeasuresAddress ="BodyMeasures/";
+    public const string MealPlanAddress = "MealPlan/";
     
 }
