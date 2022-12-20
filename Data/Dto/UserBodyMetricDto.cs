37fbdba91f894c7a8ecd6f95bcd58d1c014f263d
arielpintof <64876168+arielpintof@users.noreply.github.com>
2022-12-20 01:15:54 -0300

    Update: nullable values

--- Data/Dto/UserBodyMetricDto.cs
+++ Data/Dto/UserBodyMetricDto.cs
@@ -2,8 +2,8 @@ namespace NutrifoodsFrontend.Data.Dto;
 
 public class UserBodyMetricDto
 {
-    public int Height { get; set; }
-    public double Weight { get; set; }
+    public int? Height { get; set; }
+    public double? Weight { get; set; }
     public double BodyMassIndex { get; set; }
     public string PhysicalActivity { get; set; } = null!;
     public DateTime AddedOn { get; set; }
