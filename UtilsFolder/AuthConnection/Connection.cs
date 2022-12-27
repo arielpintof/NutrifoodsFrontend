e67f540429af8ca8fb619c7cb85113453a4c0c64
arielpintof <64876168+arielpintof@users.noreply.github.com>
2022-12-27 01:42:17 -0300

    Create: custom authentication state provider

---
+++ UtilsFolder/AuthConnection/Connection.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using NutrifoodsFrontend.UtilsFolder.ToolTip;
+
+namespace NutrifoodsFrontend.UtilsFolder.AuthConnection;
+
+public class Connection : IConnection
+{
+    private readonly AuthenticationStateProvider _authenticationStateProvider;
+    
+    public Connection(AuthenticationStateProvider authenticationStateProvider)
+    {
+        _authenticationStateProvider = authenticationStateProvider;
+    }
+
+    public async Task<AuthenticationState> GetAuthenticationState()
+    {
+       return await _authenticationStateProvider.GetAuthenticationStateAsync();
+    }
+    public async Task<string> GetApiKey()
+    {
+        var state = await GetAuthenticationState();
+        return state.User.Claims.FirstOrDefault(c => c.Type == Claims.NameIdentifier)!.Value;
+    }
+
+    public async Task<bool> IsAuthenticated()
+    {
+        var state = await GetAuthenticationState();
+        return state.User.Identity!.IsAuthenticated;
+    }
+
+    public async Task<string> GetNickName()
+    {
+        var state = await GetAuthenticationState();
+        return state.User.Claims.FirstOrDefault(c => c.Type == "nickname")!.Value;
+    }
+
+    public async Task<string> GetEmail()
+    {
+        var state = await GetAuthenticationState();
+        return state.User.Claims.FirstOrDefault(c => c.Type == "name")!.Value;
+    }
+}
